c7a912e9804273fb46b296de326ed10440cfcad5
Alvaro.Reyes@gds.ey.com <Alvaro.Reyes@gds.ey.com>
2025-10-29 22:02:36 +0000

    Added more tests

--- models/marts/dim_customers.sql
+++ models/marts/dim_customers.sql
@@ -1,18 +1,11 @@
 with customers as (
 
-    select 
-        id as customer_id,
-        first_name,
-        last_name 
-    from raw.jaffle_shop.customers
+    select * from {{ ref('stg_jaffle_shop__customers') }}
 
 ),
 
 orders as (
-    select
-        id as order_id,
-        order_date
-    from raw.jaffle_shop.orders
+    select * from {{ ref('stg_jaffle_shop__orders') }}
 
 ),
 
